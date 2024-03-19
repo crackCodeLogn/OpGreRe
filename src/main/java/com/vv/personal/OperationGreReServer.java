28ab676152f00f9a0e8b2eeed85605dc2c76f320
Vivek <vivektheintel@gmail.com>
2024-03-18 23:33:43 -0400

    Addition of Accessed and Marked practice

--- src/main/java/com/vv/personal/OperationGreReServer.java
+++ src/main/java/com/vv/personal/OperationGreReServer.java
@@ -30,20 +30,22 @@ public class OperationGreReServer {
     private static final String ROUTE_WORD_MEANING = "word-meaning";
     private static final String ROUTE_IMAGE_EXTRACTION = "image-extraction";
     private static final String ROUTE_PRACTICE_RANDOM = "practice-random";
+    private static final String ROUTE_PRACTICE_ACCESSED = "practice-accessed";
+    private static final String ROUTE_PRACTICE_MARKED = "practice-marked";
     private static final int UI_WIDTH = 1150;
     private static final int UI_HEIGHT = 600;
 
     public static void main(String[] args) {
         TimeZone.setDefault(TimeZone.getTimeZone("GMT-04:00")); //force setting
         ApplicationPropertyReader applicationProperties = new ApplicationPropertyReader(APPLICATION_PROPERTIES);
-        if (args.length == 0) {
-            System.out.println("Cannot start app as argument missing!");
-            System.exit(-1);
-        }
-        routeAndFire(args, applicationProperties);
+        OperationGreReServer operationGreReServer = new OperationGreReServer();
+        if (args.length == 0) operationGreReServer.shutdown("Cannot start app as argument missing!");
+
+        operationGreReServer.routeAndFire(args, applicationProperties);
     }
 
-    private static void routeAndFire(String[] args, ApplicationPropertyReader applicationProperties) {
+    private void routeAndFire(String[] args, ApplicationPropertyReader applicationProperties) {
+        LocalData localData = new LocalDataImpl();
         switch (args[0]) {
             case ROUTE_WORD_MEANING:
                 extractWordMeaning(applicationProperties);
@@ -51,19 +53,28 @@ public class OperationGreReServer {
             case ROUTE_IMAGE_EXTRACTION:
                 extractImages(applicationProperties);
                 break;
+            case ROUTE_PRACTICE_ACCESSED:
+                validate(args.length);
+                performAccessedPractice(Integer.parseInt(args[1]), localData);
+                break;
+            case ROUTE_PRACTICE_MARKED:
+                validate(args.length);
+                performMarkedPractice(Integer.parseInt(args[1]), localData);
+                break;
             case ROUTE_PRACTICE_RANDOM:
             default:
-                if (args.length != 2) {
-                    System.out.println("Supply the number of words you want to do for practice as the second input parameter.");
-                    System.exit(0);
-                }
-                performRandomPractice(applicationProperties, Integer.parseInt(args[1]));
+                validate(args.length);
+                performRandomPractice(applicationProperties, Integer.parseInt(args[1]), localData);
                 break;
         }
     }
 
-    private static void performRandomPractice(ApplicationPropertyReader applicationProperties, int wordsForPractice) {
-        LocalData localData = new LocalDataImpl();
+    private void validate(int argsLength) {
+        if (argsLength != 2)
+            shutdown("Supply the number of words you want to do for practice as the second input parameter.");
+    }
+
+    private void performRandomPractice(ApplicationPropertyReader applicationProperties, int wordsForPractice, LocalData localData) {
         List<String> accessedWords = localData.readAccessLogData();
         final List<String> allWordsToFetch = new ArrayList<>();
 
@@ -73,25 +84,39 @@ public class OperationGreReServer {
         Arrays.stream(files).forEach(file -> allWordsToFetch.add(file.getName()));
 
         allWordsToFetch.removeAll(accessedWords);
+        launch("RANDOM", allWordsToFetch, wordsForPractice, localData, true);
+    }
+
+    private void performAccessedPractice(int wordsForPractice, LocalData localData) {
+        List<String> accessedWords = localData.readAccessLogData();
+        if (accessedWords.isEmpty()) shutdown("Cannot launch as no accessed words yet!");
+
+        launch("ACCESSED", new ArrayList<>(accessedWords), wordsForPractice, localData, false);
+    }
+
+    private void performMarkedPractice(int wordsForPractice, LocalData localData) {
+        List<String> markedWords = localData.readMarkedWords();
+        if (markedWords.isEmpty()) shutdown("Cannot launch as no marked words yet!");
+
+        launch("MARKED", new ArrayList<>(markedWords), wordsForPractice, localData, false);
+    }
+
+    private void launch(String title, List<String> allWordsToFetch, int wordsForPractice, LocalData localData, boolean updateAccessLog) {
         Collections.shuffle(allWordsToFetch);
         List<String> allWords = allWordsToFetch.subList(0, wordsForPractice);
-        //List<Integer> randomIndices = RandomizerHelper.getRandomIndexList(wordsForPractice, 0, allWords.size());
-
         List<WordModel> wordModels = allWords.stream()
                 .map(word -> {
                     List<String> wordMeaning = localData.readWordMeaning(word);
                     ImageIcon imageIcon = localData.readImageForWord(word);
-
                     return new WordModel(word, wordMeaning, imageIcon);
-                })
-                .collect(Collectors.toList());
+                }).collect(Collectors.toList());
 
         //transfer to UI this list
         wordModels.forEach(wordModel -> LoggingHelper.info(wordModel.toString()));
-        manageUi(wordModels, "GRE PRACTISE WORDS -- RANDOM VOCAB: " + wordsForPractice, localData);
+        manageUi(wordModels, String.format("GRE PRACTISE WORDS :: %s - %d", title, wordsForPractice), localData, updateAccessLog);
     }
 
-    private static void manageUi(List<WordModel> wordModelList, String title, LocalData localData) {
+    private void manageUi(List<WordModel> wordModelList, String title, LocalData localData, boolean updateAccessLog) {
         TreeSet<String> markedWordsForLaterPractise = new TreeSet<>();
 
         JFrame jFrame = new Gui(wordModelList, 0, markedWordsForLaterPractise);
@@ -108,8 +133,10 @@ public class OperationGreReServer {
                 //this is the implementation for the writing of the marked words onto the markedWords txt file.. these words are those words which have been difficult to remember at first, so as to store them for later purpose, and revise 'em specifically..
 
                 List<String> wordsAccessed = wordModelList.stream().map(WordModel::getWord).collect(Collectors.toList());
-                localData.saveAccessLogData(wordsAccessed);
-                LoggingHelper.info(String.format("Saved access log data of %d records", wordsAccessed.size()));
+                if (updateAccessLog) {
+                    localData.saveAccessLogData(wordsAccessed);
+                    LoggingHelper.info(String.format("Saved access log data of %d records", wordsAccessed.size()));
+                }
 
                 if (!markedWordsForLaterPractise.isEmpty()) {
                     List<String> markedWords = wordModelList.stream()
@@ -119,22 +146,27 @@ public class OperationGreReServer {
                     localData.saveMarkedWords(markedWords);
                     LoggingHelper.info(String.format("Saved marked words of %d records", markedWords.size()));
                 }
-                System.exit(0);
+                shutdown("Bye");
             }
         });
     }
 
-    private static void extractImages(ApplicationPropertyReader applicationProperties) {
+    private void extractImages(ApplicationPropertyReader applicationProperties) {
         Config configuration = applicationProperties.readConfigProperties();
         ImageExtractor imageExtractor = new ImageExtractor(configuration.getImage(), configuration.getWords());
         imageExtractor.extractAbsentImagesOnly();
         imageExtractor.shutdown();
     }
 
-    private static void extractWordMeaning(ApplicationPropertyReader applicationProperties) {
+    private void extractWordMeaning(ApplicationPropertyReader applicationProperties) {
         Config configuration = applicationProperties.readConfigProperties();
         WordMeaningExtractor wordMeaningExtractor = new WordMeaningExtractor(configuration.getWord(), configuration.getWords());
         wordMeaningExtractor.extractAbsentWordMeaningsOnly();
         wordMeaningExtractor.shutdown();
+    }
+
+    private void shutdown(String shutdownMessage) {
+        LoggingHelper.info(shutdownMessage);
+        System.exit(0);
     }
 }
